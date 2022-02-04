94e77ca3e43548ed90a9ebae9d1f87c3557493e2
Riffat Nourin <rnou112@aucklanduni.ac.nz>
2022-02-04 15:21:19 +1300

    Fixed height issues in Video display

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
     <v-container class="pa-0" fluid>
       <v-row class="d-flex" align="start" no-gutters>
         <v-col cols="12" md="5" lg="4" xs="6">
-          <v-container class="pa-0" :class="$vuetify.breakpoint.mdAndUp?'full-height':''">
+          <v-container class="pa-0" :class="$vuetify.breakpoint.mdAndUp?'full-height':'auto-height'">
             <v-row class="d-flex flex-column" no-gutters>
               <v-col ref="panel">
                 <v-card 
@@ -22,12 +22,12 @@
         </v-col>
         <v-col cols="12" md="7" lg="8" xs="6">
           <v-main>
-            <v-container class="pa-0 black overflow-y-auto" :class="$vuetify.breakpoint.mdAndUp?'full-height':''">
+            <v-container class="pa-0 black overflow-y-auto" :class="$vuetify.breakpoint.mdAndUp?'full-height':'auto-height'">
               <Nuxt/>
             </v-container>
           </v-main>
         </v-col>
-      </v-row>
+      </v-row>  
       <v-row class="d-md-none" no-gutters>
         <v-col>
           <navigation/>
