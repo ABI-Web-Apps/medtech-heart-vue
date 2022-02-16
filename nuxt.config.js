364087563a57064e11cabe640fa1e6f9c49465e5
Riffat Nourin <rnou112@aucklanduni.ac.nz>
2022-02-17 00:45:55 +1300

    Added favicon related to MedTech ABI

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,6 +8,9 @@ export default {
     htmlAttrs: {
       lang: 'en'
     },
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon2.ico' }
+    ],
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
