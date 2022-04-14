50b7d1ac786238d320c442adbb8bba6ada894320
LinkunGao <gaolinkun123@gmail.com>
2022-04-14 14:32:36 +1200

    try to fix title fig

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,13 @@ export default {
     htmlAttrs: {
       lang: "en",
     },
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon2.ico" }],
+    link: [
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        href: "/medtech-heart-vue/favicon2.ico",
+      },
+    ],
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
