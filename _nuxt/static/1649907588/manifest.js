c14d8efbbfc23235eae3671c3840aa37d82eac12
LinkunGao <gaolinkun123@gmail.com>
2022-04-14 21:27:08 +1200

    fix some UI bug

--- _nuxt/static/1649907588/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fmodel-heart","\u002Ffailure-compensated","\u002Fattack-minor","\u002Fattack-healthy","\u002Ffailure-healthy","\u002Fattack-severe","\u002Felectricity-healthy","\u002Felectricity-fibrillation","\u002Ffailure-decompensated","\u002Fabout","\u002FElectricity-Healthy"]})
